de5682837278b6e8f832507130bc4c9f22b19343
Noah Gordobil Merino <ngordobi@c3r6s4.42urduliz.com>
2024-09-10 15:09:55 +0200

    correccion

--- minitalk.h
+++ minitalk.h
@@ -27,5 +27,6 @@ int		ft_putnbr(int n, int count);
 int		ft_putunbr(unsigned int n, int count);
 int		ft_puthex(unsigned long n, int count, char arg_format);
 int		ft_putptr(unsigned long p, int count);
+//sigaction()
 
 #endif
